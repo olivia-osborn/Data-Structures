519e2bd59e2d7eb957060f4e70aa840270d6cac0
Olivia Osborn <olioz95@gmail.com>
2019-05-06 17:21:26 -0400

    started setting up queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,27 @@
+class Node:
+    def __init__(self, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        pass
 
-  def len(self):
-    pass
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass
