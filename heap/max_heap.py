a81ced391525c6530b689e386e6af4bc0d2ef935
Olivia Osborn <olioz95@gmail.com>
2019-05-09 18:36:29 -0400

    heap now passes all but one test

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,7 +7,11 @@ class Heap:
         self._bubble_up(len(self.storage)-1)
 
     def delete(self):
-        pass
+        self.storage[0], self.storage[len(
+            self.storage)-1] = self.storage[len(self.storage)-1], self.storage[0]
+        removed = self.storage.pop()
+        self._sift_down(0)
+        return removed
 
     def get_max(self):
         return self.storage[0]
@@ -33,15 +37,16 @@ class Heap:
     def _sift_down(self, index):
         # loop until either the element reaches the end of the array
         # or we'll break the loop
-        while index > 0:
-            left_child = (2 * index) + 1
-            right_child = (2 * index) + 2
-            if self.storage[left_child] > self.storage[right_child] and self.storage[left_child] > self.storage[index]:
-                self.storage[index], self.storage[left_child] = self.storage[left_child], self.storage[index]
-            elif self.storage[right_child] > self.storage[left_child] and self.storage[right_child] > self.storage[index]:
-                self.storage[index], self.storage[right_child] = self.storage[right_child], self.storage[index]
-            else:
-                break
+        left_child = (2 * index) + 1
+        right_child = (2 * index) + 2
+        largest = index
+        if left_child in range(len(self.storage)-1) and self.storage[left_child] > self.storage[largest]:
+            largest = left_child
+        if right_child in range(len(self.storage)-1) and self.storage[right_child] > self.storage[largest]:
+            largest = right_child
+        if self.storage[largest] > self.storage[index]:
+            self.storage[largest], self.storage[index] = self.storage[index], self.storage[largest]
+            self._sift_down(largest)
 
     def __str__(self):
         return f'arr: {self.storage}'
@@ -50,8 +55,21 @@ class Heap:
 hp = Heap()
 hp.insert(5)
 hp.insert(9)
+hp.insert(15)
 hp.insert(4)
+hp.insert(45)
 hp.insert(22)
-hp.insert(13)
+
+hp.delete()
+
+# hp.insert(6)
+# hp.insert(8)
+# hp.insert(10)
+# hp.insert(9)
+# hp.insert(1)
+# hp.insert(9)
+# hp.insert(9)
+# hp.insert(5)
+# hp.delete()
 
 print(hp)
